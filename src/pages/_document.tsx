02e405603516ab545cb0888676f15e78a9762cd8
Nikolay Lanets <n.lanets@freecode.academy>
2026-05-02 22:23:25 +0300

    Added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="icon" type="image/png" href="/favicon.png"></link>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
